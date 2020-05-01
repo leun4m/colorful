764d3bf9e1e32e8c410eb57507907ca56e136437
Manuel Krebs <m4nuelkrebs@gmail.com>
2020-05-01 12:28:36 +0200

    Add from_hex()

--- src/color.rs
+++ src/color.rs
@@ -4,7 +4,7 @@
 pub struct Color {
     red: u8,
     green: u8,
-    blue: u8
+    blue: u8,
 }
 
 impl Color {
@@ -12,13 +12,46 @@ impl Color {
         Color {
             red: r,
             green: g,
-            blue: b
+            blue: b,
         }
+    }
+
+    pub fn from_hex(hex: &str) -> Color {
+        let length = hex.chars().count();
+        if length == 6 {
+            let r = &hex[0..2];
+            let g = &hex[2..4];
+            let b = &hex[4..6];
+            Color::from_rgb(
+                Color::convert_u8_hex(r),
+                Color::convert_u8_hex(g),
+                Color::convert_u8_hex(b),
+            )
+        } else {
+            panic!("HEX number has invalid length: {}", length);
+        }
+    }
+
+    fn convert_u8_hex(hex: &str) -> u8 {
+        u8::from_str_radix(hex, 16).expect(format!("HEX no valid u8: {}", hex).as_str())
     }
 }
 
 impl PartialEq for Color {
     fn eq(&self, other: &Self) -> bool {
         self.red == other.red && self.green == other.green && self.blue == other.blue
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::color::Color;
+
+    use crate::common_colors::CommonColors;
+
+    #[test]
+    fn hex_white() {
+        let color = Color::from_hex("ffffff");
+        assert_eq!(CommonColors::WHITE.as_color(), color);
     }
 }
--- src/common_colors.rs
+++ src/common_colors.rs
@@ -1,26 +1,30 @@
 use crate::color::Color;
 
-enum CommonColors {
+#[derive(Debug)]
+pub enum CommonColors {
     WHITE,
-    BLACK
+    BLACK,
 }
 
 impl CommonColors {
     pub fn as_color(&self) -> Color {
         match &self {
             CommonColors::WHITE => Color::from_rgb(255, 255, 255),
-            CommonColors::BLACK => Color::from_rgb(0,0,0)
+            CommonColors::BLACK => Color::from_rgb(0, 0, 0),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::common_colors::CommonColors;
     use crate::color::Color;
+    use crate::common_colors::CommonColors;
 
     #[test]
     fn white_is_white() {
-        assert_eq!(Color::from_rgb(255,255,255), CommonColors::WHITE.as_color(),)
+        assert_eq!(
+            Color::from_rgb(255, 255, 255),
+            CommonColors::WHITE.as_color(),
+        )
     }
 }
