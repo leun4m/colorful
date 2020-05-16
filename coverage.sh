1033a6e173ee04a8ca4e9fe131c288ffbab88fb3
Manuel Krebs <m4nuelkrebs@gmail.com>
2020-05-16 15:22:14 +0200

    Remove coverage.sh

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm target/debug/deps -r
-cargo build
-cargo test
-grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
-xdg-open target/debug/coverage/index.html
-
